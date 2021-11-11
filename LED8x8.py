ee0da2db2243ec37fc05ee49b8665f8e181be645
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-11 19:44:53 +0000

    all off

--- LED8x8.py
+++ LED8x8.py
@@ -2,14 +2,14 @@ import time
 from shifter import Shifter
 
 class led8x8():
-  pattern = [0b11111111, 0b11111111,0b11111111, 0b11111111,0b00000000, 0b00000000,0b00000000, 0b00000000]
+  pattern = [0b11111111, 0b11111111,0b11111111, 0b11111111,0b11111111, 0b11111111,0b11111111, 0b11111111]
   rows = [0b00000001, 0b00000010,0b00000100, 0b00001000,0b00010000, 0b00100000,0b01000000, 0b10000000]
   def __init__(self,data,latch,clock):
     self.shifter = Shifter(data,latch,clock)
   def display(self):
     for n in range(8):
-      self.shifter.shiftByte(led8x8.rows[0])
-      self.shifter.shiftByte(led8x8.pattern[0])
+      self.shifter.shiftByte(led8x8.rows[n])
+      self.shifter.shiftByte(led8x8.pattern[n])
       
 
       
