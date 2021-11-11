edcb0e55c8da00f1c827687c718d2024a3759e23
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-11 20:11:41 +0000

    list

--- LED8x8.py
+++ LED8x8.py
@@ -5,7 +5,8 @@ class led8x8():
   pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,0b10100101, 0b10011001, 0b01000010, 0b00111100]
   rows = [0b00000001, 0b00000010,0b00000100, 0b00001000,0b00010000, 0b00100000,0b01000000, 0b10000000]
   mask = 0b11111111
-  pattern = (~pattern) & mask
+  for n in range(8):
+    pattern[n] = (~pattern[n]) & mask
   def __init__(self,data,latch,clock):
     self.shifter = Shifter(data,latch,clock)
   def display(self):
