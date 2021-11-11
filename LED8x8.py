ba34597e0258c7fee52c9e3a49f2b4b87e6acbf3
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-11 19:30:14 +0000

    off

--- LED8x8.py
+++ LED8x8.py
@@ -8,7 +8,7 @@ class led8x8():
     self.shifter = Shifter(data,latch,clock)
   def display(self):
     for n in range(8):
-      self.shifter.shiftByte(led8x8.pattern[n])
+      self.shifter.shiftByte(led8x8.pattern[0])
       self.shifter.shiftByte(1<<(n))
 
       
