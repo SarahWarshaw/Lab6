5f025e542b5c9d78178cc600f1be4ad14fba18d6
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-12 18:37:00 +0000

    self

--- LED8x8.py
+++ LED8x8.py
@@ -3,13 +3,13 @@ from shifter import Shifter
 import multiprocessing
 
 class led8x8():
+  pattern = multiprocessing.Array('i',8)
   # pattern2 = [0b10001110,0b01101100,0b01101010,0b10001010,0b10001010,0b01101010,0b01101100,0b10001110] #DB pattern
   rows = [0b00000001, 0b00000010,0b00000100, 0b00001000,0b00010000, 0b00100000,0b01000000, 0b10000000]
   cols = [0b00000001, 0b00000010,0b00000100, 0b00001000,0b00010000, 0b00100000,0b01000000, 0b10000000]
   mask = 0b11111111
   def __init__(self,data,latch,clock):
     self.shifter = Shifter(data,latch,clock)
-    pattern = multiprocessing.Array('i',8)
     p = multiprocessing.Process(target = self.smiley,args = (self.pattern,))
     p.daemon = True
     p.start()
