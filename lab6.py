b3ac0b2040eaca2ac16de0a6be3f8b318ee53eae
Sarah Warshawsky <91498827+SarahWarshaw@users.noreply.github.com>
2021-11-12 18:04:46 +0000

    lets try this

--- lab6.py
+++ lab6.py
@@ -1,11 +1,28 @@
 import time 
 from LED8x8 import led8x8
+import multiprocessing
+import random
 
 dataPin, latchPin, clockPin = 23,24,25
 
 myLED8x8 = led8x8(dataPin, latchPin, clockPin)
 
+def newPattern(myPattern):
+  move = random.randint(0,1)
+  
+  pattern = [0b10001110,0b01101100,0b01101010,0b10001010,0b10001010,0b01101010,0b01101100,0b10001110]
+  pattern[0] = move
+  for n in range(8):
+    myPattern.Array[n] = pattern[n]
+
 while True:
-  myLED8x8.display()
+  myPattern = multiprocessing.Array('i')
+  myPattern = newPattern(myPattern)
+  p = multiprocessing.Process(target = led8x8.display,args = myPattern)
+  p.daemon = True
+  p.start
+  # pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,0b10100101, 0b10011001, 0b01000010, 0b00111100]
+  myPattern = newPattern(myPattern)
   time.sleep(0.0001)
+  p.join()
 
---
+++ main.py
@@ -0,0 +1,8 @@
+# display running constantly
+
+# displays what ever is in the pattern
+
+# run something else thats changing pattern
+
+# make lightning bug work as separate process
+
